29908e06c5cd37d8a9e4e4cf0d9802eae7129404
lamecode <kislaya.sliva@gmail.com>
2020-12-10 09:08:53 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //before optimization
         long start = System.nanoTime();
         int[][] MB = new int[10][10];
         int res = 0;
@@ -10,5 +11,17 @@ public class Main {
         }
         long end = System.nanoTime();
         System.out.println("Program execution time - " + (double) (end - start) / Math.pow(10, 9));
+        //after optimization
+        long start1 = System.nanoTime();
+        int[][] MB1 = new int[10][10];
+        int[] row;
+        for (int i = 0; i < 10; i++) {
+            row = MB[i];
+            for (int j = 0; j < row.length; j++) {
+                row[j]++;
+            }
+        }
+        long end1 = System.nanoTime();
+        System.out.println("Program execution time - " + (double) (end1 - start1) / Math.pow(10, 9));
     }
 }
